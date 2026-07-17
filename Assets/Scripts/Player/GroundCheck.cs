6134261d1d35c445db187be2a10bfb2f882076c4
isikai49 <isikai49@gmail.com>
2026-07-17 20:44:44 +0900

    AnimationCurveの追加、プログラムの整理

--- Assets/Scripts/Player/GroundCheck.cs
+++ Assets/Scripts/Player/GroundCheck.cs
@@ -1,37 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GroundCheck : MonoBehaviour
 {
+    #region//プライベート変数
     private string groundTag = "Ground";
-    public bool isGround = false;
+    private bool isGround = false;
+    private bool isGroundEnter, isGroundStay, isGroundExit;
+    #endregion
 
+    //接地判定を返すメソッド
+    //物理判定の更新毎に呼ぶ必要がある
     public bool IsGround()
     {
+        if (isGroundEnter || isGroundStay)
+        {
+            isGround = true;
+        }
+        else if (isGroundExit)
+        {
+            isGround = false;
+        }
+
+        isGroundEnter = false;
+        isGroundStay = false;
+        isGroundExit = false;
         return isGround;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag(groundTag))
+        if (collision.tag == groundTag)
         {
-            isGround = true;
+            isGroundEnter = true;
             Debug.Log("地面に接地");
         }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.CompareTag(groundTag))
+        if (collision.tag == groundTag)
         {
-            isGround = true;
+            isGroundStay = true;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag(groundTag))
+        if (collision.tag == groundTag)
         {
-            isGround = false;
+            isGroundExit = true;
             Debug.Log("地面から離れた");
         }
     }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,36 +1,44 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    //インスペクターで設定する変数
-    public float speed;//速度
-    public float gravity;//重力
-    public float jumpSpeed;//ジャンプ速度
-    public float jumpLimitTime;//ジャンプ制限時間
-    public GroundCheck ground;//設置判定
-    public float jumpHeight;//ジャンプ高さ
-    public GroundCheck head;//頭ぶつけた判定
+    #region//インスペクターで設定する
+    [Header("移動速度")] public float speed;
+    [Header("重力")] public float gravity;
+    [Header("ジャンプ速度")] public float jumpSpeed;
+    [Header("ジャンプ制限時間")] public float jumpLimitTime;
+    [Header("設置判定")] public GroundCheck ground;
+    [Header("ジャンプ高さ")] public float jumpHeight;
+    [Header("頭ぶつけた判定")] public GroundCheck head;
+    [Header("ダッシュの速さ表現")] public AnimationCurve dashCurve;
+    [Header("ジャンプの速さ表現")] public AnimationCurve jumpCurve;
+    #endregion
 
-    //プライベート変数
+    #region//プライベート変数
     private Animator anim;
     private Rigidbody2D rb;
     private bool isGround = false;
+    private bool isRun = false;
     private bool isHead = false;
     private bool isJump = false;
     private bool jumpPressed = false;
     private float jumpPos = 0.0f;
+    private float dashTime = 0.0f;
     private float jumpTime = 0.0f;
+    private float beforeKey = 0.0f;
+    #endregion
 
     private void Start()
     {
+        //コンポーネントのインスタンスを捕まえる
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        // ↑キーを押した瞬間だけtrue
+        // スペースキーを押した瞬間だけtrue
         if (Input.GetKeyDown(KeyCode.Space))
         {
             jumpPressed = true;
@@ -43,10 +51,23 @@ public class PlayerController : MonoBehaviour
         isGround = ground.IsGround();
         isHead = head.IsGround();
 
-        //キー入力されたら行動する
-        float horizontalKey = Input.GetAxis("Horizontal");
+        //各種座標軸の速度を求める
+        float xSpeed = GetXSpeed();
+        float ySpeed = GetYSpeed();
 
-        float xSpeed = 0.0f;
+        //アニメーションを適用
+        SetAnimation();
+
+        //移動速度を設定
+        rb.linearVelocity = new Vector2(xSpeed, ySpeed);
+    }
+
+    /// <summary> 
+    /// Y成分で必要な計算をし、速度を返す。 
+    /// </summary> 
+    /// <returns>Y軸の速さ</returns> 
+    private float GetYSpeed()
+    {
         float ySpeed = -gravity;
 
         if (isGround)
@@ -98,27 +119,70 @@ public class PlayerController : MonoBehaviour
             jumpPressed = false;
         }
 
+        if (isJump)
+        {
+            ySpeed *= jumpCurve.Evaluate(jumpTime);
+        }
+
+        return ySpeed;
+    }
+
+    /// <summary> 
+    /// X成分で必要な計算をし、速度を返す。 
+    /// </summary> 
+    /// <returns>X軸の速さ</returns> 
+    private float GetXSpeed()
+    {
+        float horizontalKey = Input.GetAxis("Horizontal");
+        float xSpeed = 0.0f;
+
+
+
         if (horizontalKey > 0)
         {
             transform.localScale = new Vector3(1, 1, 1);
-            anim.SetBool("run", true);
+            isRun = true;
+            dashTime += Time.deltaTime;
             xSpeed = speed;
         }
         else if (horizontalKey < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
-            anim.SetBool("run", true);
+            isRun = true;
+            dashTime += Time.deltaTime;
             xSpeed = -speed;
         }
         else
         {
-            anim.SetBool("run", false);
+            isRun = false;
             xSpeed = 0.0f;
+            dashTime = 0.0f;
         }
 
+        //前回の入力からダッシュの反転を判断して速度を変える
+        if (horizontalKey > 0 && beforeKey < 0)
+        {
+            dashTime = 0.0f;
+        }
+        else if (horizontalKey < 0 && beforeKey > 0)
+        {
+            dashTime = 0.0f;
+        }
+
+        beforeKey = horizontalKey;
+        xSpeed *= dashCurve.Evaluate(dashTime);
+        beforeKey = horizontalKey;
+
+        return xSpeed;
+    }
+
+    /// <summary> 
+    /// アニメーションを設定する 
+    /// </summary> 
+    private void SetAnimation()
+    {
         anim.SetBool("jump", isJump);
         anim.SetBool("ground", isGround);
-
-        rb.linearVelocity = new Vector2(xSpeed, ySpeed);
+        anim.SetBool("run", isRun);
     }
 }
